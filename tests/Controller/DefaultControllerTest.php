fff899236ae7cb046973f937db08963ef8005a61
Fabien Stenneler <53312455+fstenneler@users.noreply.github.com>
2019-12-17 15:50:38 +0100

    added functionnal tests

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\App\Controller;
+
+use App\Entity\User;
+use App\Tests\Controller\AuthenticationUtil;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    private $client = null;
+    private $auth;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $this->auth = new AuthenticationUtil($this->client);
+    }
+
+    public function testIndexRedirection()
+    {
+
+        // test redirection to login page
+        $this->client->request('GET', '/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(true, $this->client->getResponse()->isRedirect('/login'));
+
+        // test response after redirection
+        $this->client->request('GET', '/');
+        $this->client->followRedirect();
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+    }
+
+    public function testIndexContent()
+    {
+        $this->auth->logIn();
+        $crawler = $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('Consulter la liste des tâches à faire', $crawler->text());
+    }
+
+}
